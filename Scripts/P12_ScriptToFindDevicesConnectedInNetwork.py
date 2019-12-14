e76f2fc7de8f1cd2e6cba17c206eba88726b8425
Akhilesh Santoshwar <58439678+akhilesh1806@users.noreply.github.com>
2019-12-14 21:29:34 +0530

    Update P12_ScriptToFindDevicesConnectedInNetwork.py

--- Scripts/P12_ScriptToFindDevicesConnectedInNetwork.py
+++ Scripts/P12_ScriptToFindDevicesConnectedInNetwork.py
@@ -4,19 +4,15 @@
 
 # This script needs python-nmap as a pre-requisite. To install: sudo pip3 install python-nmap
 
-import nmap
-import subprocess
+import subprocess 
 
-# function to scan network and display IPs of conected devices
-def scan_network():
-    scanner = nmap.PortScanner()
-    myIP = subprocess.check_output(['hostname -I'], shell=True)
-    myIP = str(myIP, 'utf-8').split('.')
-    print(myIP[:3])
-    scannedData = scanner.scan(hosts = '.'.join(myIP[:3]) + '.1/24', arguments = '-sP')
+for ping in range(1,10): 
+	address = "127.0.0." + str(ping) 
+	res = subprocess.call(['ping', '-c', '3', address]) 
+	if res == 0: 
+		print( "ping to", address, "OK") 
+	elif res == 2: 
+		print("no response from", address) 
+	else: 
+		print("ping to", address, "failed!") 
 
-    # printing all the IP addresses of connected devices
-    for hostnames in scannedData['scan']:
-        print(hostnames)
-
-scan_network()
