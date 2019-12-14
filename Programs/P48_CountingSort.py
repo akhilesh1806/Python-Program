4a7ddab9f819410b0fb2f77b0939d6cdbace79cf
Akhilesh Santoshwar <58439678+akhilesh1806@users.noreply.github.com>
2019-12-14 19:56:44 +0530

    Update P48_CountingSort.py

--- Programs/P48_CountingSort.py
+++ Programs/P48_CountingSort.py
@@ -28,25 +28,22 @@
 #  where n is the size of the input array and k means the
 #  values range from 0 to k.
 
-def countingSort(myList):
-    maxValue = 0
-    for i in range(len(myList)):
-        if myList[i] > maxValue:
-            maxValue = myList[i]
-
-    buckets = [0] * (maxValue + 1)
-
-    for i in myList:
-        buckets[i] += 1
-
-    i = 0
-    for j in range(maxValue + 1):
-         for a in range(buckets[j]):
-             myList[i] = j
-             i += 1
-
-    return myList
-
-if __name__ == '__main__':
-    sortedList = countingSort([1,23,4,5,6,7,8])
-    print(sortedList)
+def countingSort(array):
+    size = len(array)
+    output = [0] * size
+    count = [0] * 10
+    for i in range(0, size):
+        count[array[i]] += 1
+    for i in range(1, 10):
+        count[i] += count[i - 1]
+    i = size - 1
+    while i >= 0:
+        output[count[array[i]] - 1] = array[i]
+        count[array[i]] -= 1
+        i -= 1
+    for i in range(0, size):
+        array[i] = output[i]
+data = [4, 2, 2, 8, 3, 3, 1]
+countingSort(data)
+print("Sorted Array in Ascending Order: ")
+print(data)
