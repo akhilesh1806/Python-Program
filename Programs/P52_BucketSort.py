edce010776660a1cd1136d6c8b155a9d312ada0d
Akhilesh Santoshwar <58439678+akhilesh1806@users.noreply.github.com>
2019-12-14 19:59:32 +0530

    Update P52_BucketSort.py

--- Programs/P52_BucketSort.py
+++ Programs/P52_BucketSort.py
@@ -12,44 +12,24 @@
 #  Time Complexity of Solution:
 #  Best Case O(n); Average Case O(n); Worst Case O(n)
 
-from P26_InsertionSort import insertionSort
-import math
-
-DEFAULT_BUCKET_SIZE = 5
-
-def bucketSort(myList, bucketSize=DEFAULT_BUCKET_SIZE):
-    if(len(myList) == 0):
-        print('You don\'t have any elements in array!')
-
-    minValue = myList[0]
-    maxValue = myList[0]
-
-    # For finding minimum and maximum values
-    for i in range(0, len(myList)):
-        if myList[i] < minValue:
-            minValue = myList[i]
-        elif myList[i] > maxValue:
-            maxValue = myList[i]
-
-    # Initialize buckets
-    bucketCount = math.floor((maxValue - minValue) / bucketSize) + 1
-    buckets = []
-    for i in range(0, bucketCount):
-        buckets.append([])
-
-    # For putting values in buckets
-    for i in range(0, len(myList)):
-        buckets[math.floor((myList[i] - minValue) / bucketSize)].append(myList[i])
-
-    # Sort buckets and place back into input array
-    sortedArray = []
-    for i in range(0, len(buckets)):
-        insertionSort(buckets[i])
-        for j in range(0, len(buckets[i])):
-            sortedArray.append(buckets[i][j])
-
-    return sortedArray
-
-if __name__ == '__main__':
-    sortedArray = bucketSort([12, 23, 4, 5, 3, 2, 12, 81, 56, 95])
-    print(sortedArray)
+def bucketSort(array): 
+    bucket = [] 
+    for i in range(len(array)): 
+        bucket.append([])
+        
+    for j in array: 
+        index_b = int(10 * j) 
+        bucket[index_b].append(j) 
+    
+    for i in range(len(array)): 
+        bucket[i] = sorted(bucket[i])
+        
+    k = 0
+    for i in range(len(array)): 
+        for j in range(len(bucket[i])): 
+            array[k] = bucket[i][j] 
+            k += 1
+    return array 
+array = [.42, .32, .33, .52, .37, .47, .51] 
+print("Sorted Array in descending order is") 
+print(bucketSort(array))
